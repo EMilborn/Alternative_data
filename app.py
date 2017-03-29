49c52d75a9e6d8140df816850886c82e7785e08e
katelyj <kjohnston@stuy.edu>
2017-03-29 14:51:26 -0400

    flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.secret_key = "eliasgoestostanford"
+
+
+@app.route("/")
+def root():
+    return render_template("page.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
