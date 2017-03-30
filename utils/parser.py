4c32cf8657c9c754fc191f63cc956a255f618502
katelyj <kjohnston@stuy.edu>
2017-03-30 15:32:56 -0400

    made dictionaries

--- utils/parser.py
+++ utils/parser.py
@@ -1,16 +1,34 @@
 import csv
 
+
 data = open("../data/data.csv", "r")
 reader = csv.reader(data)
 highschool = {}
 middleschool = {}
 
-for row in reader:
-    
-    if ( row[17] == "Overall" and row[5] == "Smoking Status" and row[7] == "Ever"):
 
-        if ( row[20] == "High School" ):
-            print row[0] + ": " + row[2]
+def constructDicts():
 
-        if ( row[20] == "Middle School" ):
-            print row[0] + ": " + row[2]
+    # row[0] = year, row[2] = state, row[10] = percentage
+
+    for row in reader:
+        if ( row[17] == "Overall" and row[5] == "Smoking Status" and row[7] == "Ever"):
+
+            #high school students only
+            if ( row[20] == "High School" ):
+                if row[2] in highschool.keys(): #to avoid overwriting data
+                    highschool[row[2]].append(row[0])
+                    highschool[row[2]].append(row[10])
+                else:
+                    highschool[row[2]] = [row[0],row[10]]
+
+            #middle school students only
+            if ( row[20] == "Middle School" ):
+                if row[2] in middleschool.keys(): #to avoid overwriting data
+                    middleschool[row[2]].append(row[0])
+                    middleschool[row[2]].append(row[10])
+                else:
+                    middleschool[row[2]] = [row[0],row[10]]
+
+                
+constructDicts()
