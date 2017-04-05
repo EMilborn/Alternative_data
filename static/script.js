4e4409edc88c643970fc8a057aec2cc93b71e65e
ThisIsMyNick <nobel.gautam@gmail.com>
2017-04-05 02:42:04 -0400

    finish

--- static/script.js
+++ static/script.js
@@ -14,16 +14,19 @@ var timeText = document.getElementById("timeText");
 
 var addStates = function(){
     var width = 50;
+
     var states = [
         ["Alabama", 7, 6],
+        ["Alaska", 0, 0],
         ["Arizona", 2, 5],
         ["Arkansas", 5, 5],
         ["California", 1, 4],
         ["Colorado", 3, 4],
         ["Connecticut", 10, 3],
-        ["Delaware", 9, 5],
+        ["Delaware", 10, 4],
+        ["District of Columbia", 9, 5],
         ["Florida", 9, 7],
-	["Georgia", 8, 6],
+        ["Georgia", 8, 6],
         ["Hawaii", 0, 7],
         ["Idaho", 2, 2],
         ["Illinois", 6, 2],
@@ -34,7 +37,7 @@ var addStates = function(){
         ["Louisiana", 5, 6],
         ["Maine", 11, 0],
         ["Maryland", 9, 4],
-        ["Massachusetts", 11, 2]
+        ["Massachusetts", 11, 2],
         ["Michigan", 8, 2],
         ["Minnesota", 5, 2],
         ["Mississippi", 6, 6],
@@ -64,27 +67,25 @@ var addStates = function(){
         ["West Virginia", 7, 4],
         ["Wisconson", 7, 2],
         ["Wyoming", 3, 3],
-        ["Alaska", 0, 0],
     ];
- 
-    for (i = 0; i < 50; i++){
-    var state = states[i];
-    var newState = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    newState.setAttribute("id", state[0]);
-    newState.setAttribute("width", String(width));
-    newState.setAttribute("height", String(width));
-    newState.setAttribute("x", String(width/2 + width*state[1]));
-    newState.setAttribute("y", String(width/2 + width*state[2]));
-    newState.setAttribute("stroke", "white");
-    s.appendChild(newState);
+
+    for (i = 0; i < states.length; i++){
+        var state = states[i];
+        var newState = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        newState.setAttribute("id", state[0]);
+        newState.setAttribute("width", String(width));
+        newState.setAttribute("height", String(width));
+        newState.setAttribute("x", String(width/2 + width*state[1]));
+        newState.setAttribute("y", String(width/2 + width*state[2]));
+        newState.setAttribute("stroke", "white");
+        s.appendChild(newState);
     };
 };
 
 var resetSlider = function(){
-
     slider.value = slider.getAttribute("min");
     timeText.innerHTML = slider.value;
-        
+    retrieveData();
 }
 
 var playForward = function(){
@@ -102,36 +103,35 @@ var playBackward = function(){
 var masterControl = function(dir){
 
     if (!playing){
-	if (slider.value == slider.getAttribute("max")){
-	    if (dir == 1) resetSlider();
-	    else playBtnIc.setAttribute("class", "glyphicon glyphicon-play");
-	}
-	
-	if (dir == 1)	    playForward();
-	else if (dir == -1) playBackward();
+        if (slider.value == slider.getAttribute("max")){
+            if (dir == 1) resetSlider();
+            else playBtnIc.setAttribute("class", "glyphicon glyphicon-play");
+        }
+
+        if (dir == 1)	    playForward();
+        else if (dir == -1) playBackward();
     }
-    
+
     else if (playing == 1){
-	playBtnIc.setAttribute("class", "glyphicon glyphicon-play");
-	if (dir ==  1) playing = 0;
-	else {
-	    playing = -1;
-	    playBackward();
-	}
+        playBtnIc.setAttribute("class", "glyphicon glyphicon-play");
+        if (dir ==  1) playing = 0;
+        else {
+            playing = -1;
+            playBackward();
+        }
     }
 
     else if (playing == -1){
-	rewindBtnIc.setAttribute("class", "glyphicon glyphicon-backward");
-	if (dir == 1) {
-	    playing = 1;
-	    playForward();
-	}
-	else playing = 0;
+        rewindBtnIc.setAttribute("class", "glyphicon glyphicon-backward");
+        if (dir == 1) {
+            playing = 1;
+            playForward();
+        }
+        else playing = 0;
 
     }
 
     else console.log("that's not good");
-    
 }
 
 var play = function(){
@@ -140,56 +140,54 @@ var play = function(){
 
     //console.log("max = " + slider.getAttribute("max"));
     var anim = function(){setTimeout(function(){
-	    
-	slider.stepUp();
-	timeText.innerHTML = slider.value;
 
-	if (playing == 1 && slider.value < slider.getAttribute("max")){
+        slider.stepUp();
+        timeText.innerHTML = slider.value;
 
-	    rid = window.requestAnimationFrame(anim);
+        if (playing == 1 && slider.value < slider.getAttribute("max")){
 
-	}
+            rid = window.requestAnimationFrame(anim);
 
-	if (slider.value == slider.getAttribute("max")){
+        }
 
-	    masterControl(1);
-	    playBtnIc.setAttribute("class", "glyphicon glyphicon-repeat");
-	    
-	}
-	
+        if (slider.value == slider.getAttribute("max")){
+
+            masterControl(1);
+            playBtnIc.setAttribute("class", "glyphicon glyphicon-repeat");
+
+        }
+        retrieveData();
     }, frameTime);}
 
     anim();
-    
+
 }
 
-
-
 var rewind = function(){
-    
+
     window.cancelAnimationFrame(rid);
 
     var anim = function(){setTimeout(function(){
-	    
-	slider.stepDown();
-	timeText.innerHTML = slider.value;
 
-	if (playing == -1 && slider.value > slider.getAttribute("min")){
+        slider.stepDown();
+        timeText.innerHTML = slider.value;
 
-	    rid = window.requestAnimationFrame(anim);
+        if (playing == -1 && slider.value > slider.getAttribute("min")){
 
-	}
+            rid = window.requestAnimationFrame(anim);
 
-	if (slider.value == slider.getAttribute("min")){
+        }
 
-	    masterControl(-1);
-	    
-	}
-	
+        if (slider.value == slider.getAttribute("min")){
+
+            masterControl(-1);
+
+        }
+        retrieveData();
     }, frameTime);}
 
     anim();
-    
+
 }
 
 var playButton = function(){
@@ -205,35 +203,41 @@ rewindBtn.addEventListener("click", rewindButton);
 playBtn.addEventListener("click", playButton);
 
 
-var dataBank = {};
+var data = {};
 
-//FOR NOBEL>>>
 var getSlider = function(){
     return slider.value;
 }
-//DATA RETRIEVAL
-var retrieveData = function(){
-    $.ajax({
-	url: "/getCollege",
-	type: "GET",
-	data: {"year": getSlider()},
-	success: function(d){
-	    dataBank["College"] = JSON.parse(d);
-	}
-    });
-    $.ajax({
-	url: "/getHS",
-	type: "GET",
-	data: {"year": getSlider()},
-	success: function(d){
-	    dataBank["HS"] = JSON.parse(d);
-	}
-    });
-    
-    console.log(dataBank);
+
+var getDataset = function(){
+    if (hsRadio.checked) return 0;
+    else if (colRadio.checked) return 1;
+    else return -1;
 }
 
-
+var retrieveData = function(){
+    if (getDataset() == 0) {
+        $.ajax({
+            url: "/getHS",
+            type: "GET",
+            data: {"year": getSlider()},
+            success: function(d){
+                data = JSON.parse(d);
+                loadMap();
+            }
+        });
+    } else {
+        $.ajax({
+            url: "/getCollege",
+            type: "GET",
+            data: {"year": getSlider()},
+            success: function(d){
+                data = JSON.parse(d);
+                loadMap();
+            }
+        });
+    }
+};
 
 var onLoadFunctions = function(){
     resetSlider();
@@ -244,13 +248,58 @@ var onLoadFunctions = function(){
 
 window.onLoad = onLoadFunctions();
 
+$("input[type=radio]").on("change", function() {
+    retrieveData();
+});
 
+$("#timeSlider").on("change", function() {
+    retrieveData();
+    timeText.innerHTML = slider.value;
+});
 
-//0 for highschool, 1 for college, 2 for both
-var getDataset = function(){
-    if (hsRadio.checked) return 0;
-    else if (colRadio.checked) return 1;
-    else return 2;
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+var loadMap = function() {
+    var color;
+    if (getDataset() == 0) {
+        color = "rgb(255, 0, 0)";
+    } else {
+        color = "rgb(0, 0, 255)";
+    }
+    var formatted = []
+    var keys = Object.keys(data).sort();
+    var values = keys.map(k => parseFloat(data[k]))
+    for (var i = 0; i < keys.length; i++) {
+        formatted.push({
+            "state": keys[i],
+            "data": data[keys[i]]
+        });
+    }
+    var opacityScale = d3.scale.linear()
+        .domain([d3.min(values), d3.max(values)])
+        .range([0.1, 1])
+    d3.select("svg").selectAll("rect")
+        .data(formatted)
+        .attr("fill", function(d, i) {
+            return color;
+        })
+        .attr("fill-opacity", function(d, i) {
+            return opacityScale(d["data"])
+        })
+        .on("mouseover", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div .html(d["state"] + "<br>" + d["data"] + "%")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        })
+        .enter()
 }
-
-
