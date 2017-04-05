d94c495e56fa8a8cc79d106ba2ab19012bf0215c
Elias Milborn <emilborn@stuy.edu>
2017-04-04 23:55:57 -0400

    reordered states

--- static/script.js
+++ static/script.js
@@ -14,69 +14,70 @@ var timeText = document.getElementById("timeText");
 
 var addStates = function(){
     var width = 50;
-    var states = [["Alaska", 0, 0],
-		  ["Hawaii", 0, 7],
-		  ["Washington", 1, 2],
-		  ["Oregon", 1, 3],
-		  ["California", 1, 4],
-		  ["Idaho", 2, 2],
-		  ["Nevada", 2, 3],
-		  ["Utah", 2, 4],
-		  ["Arizona", 2, 5],
-		  ["Montana", 3, 2],
-		  ["Wyoming", 3, 3],
-		  ["Colorado", 3, 4],
-		  ["New Mexico", 3, 5],
-		  ["North Dakota", 4, 2],
-		  ["South Dakota", 4, 3],
-		  ["Nebraska", 4, 4],
-		  ["Kansa", 4, 5],
-		  ["Oklahoma", 4, 6],
-		  ["Texas", 4, 7],
-		  ["Minnesota", 5, 2],
-		  ["Iowa", 5, 3],
-		  ["Missouri", 5, 4],
-		  ["Arkansas", 5, 5],
-		  ["Louisiana", 5, 6],
-		  ["Illinois", 6, 2],
-		  ["Indiana", 6, 3],
-		  ["Kentucky", 6, 4],
-		  ["Tennessee", 6, 5],
-		  ["Mississippi", 6, 6],
-		  ["Wisconson", 7, 2],
-		  ["Ohio", 7, 3],
-		  ["West Virginia", 7, 4],
-		  ["North Carolina", 7, 5],
-		  ["Alabama", 7, 6],
-		  ["Michigan", 8, 2],
-		  ["Pennsylvania", 8, 3],
-		  ["Virginia", 8, 4],
-		  ["South Carolina", 8, 5],
-		  ["Alabama", 8, 6],
-		  ["New York", 9, 2],
-		  ["New Jersey", 9, 3],
-		  ["Maryland", 9, 4],
-		  ["Delaware", 9, 5],
-		  ["Florida", 9, 7],
-		  ["Vermont", 10, 1],
-		  ["Rhode Island", 10, 2],
-		  ["Connecticut", 10, 3],
-		  ["Maine", 11, 0],
-		  ["New Hampshire", 11, 1],
-		  ["Massachusetts", 11, 2]
-		 ];
-    
+    var states = [
+        ["Alabama", 7, 6],
+        ["Arizona", 2, 5],
+        ["Arkansas", 5, 5],
+        ["California", 1, 4],
+        ["Colorado", 3, 4],
+        ["Connecticut", 10, 3],
+        ["Delaware", 9, 5],
+        ["Florida", 9, 7],
+	["Georgia", 8, 6],
+        ["Hawaii", 0, 7],
+        ["Idaho", 2, 2],
+        ["Illinois", 6, 2],
+        ["Indiana", 6, 3],
+        ["Iowa", 5, 3],
+        ["Kansa", 4, 5],
+        ["Kentucky", 6, 4],
+        ["Louisiana", 5, 6],
+        ["Maine", 11, 0],
+        ["Maryland", 9, 4],
+        ["Massachusetts", 11, 2]
+        ["Michigan", 8, 2],
+        ["Minnesota", 5, 2],
+        ["Mississippi", 6, 6],
+        ["Missouri", 5, 4],
+        ["Montana", 3, 2],
+        ["Nebraska", 4, 4],
+        ["Nevada", 2, 3],
+        ["New Hampshire", 11, 1],
+        ["New Jersey", 9, 3],
+        ["New Mexico", 3, 5],
+        ["New York", 9, 2],
+        ["North Carolina", 7, 5],
+        ["North Dakota", 4, 2],
+        ["Ohio", 7, 3],
+        ["Oklahoma", 4, 6],
+        ["Oregon", 1, 3],
+        ["Pennsylvania", 8, 3],
+        ["Rhode Island", 10, 2],
+        ["South Carolina", 8, 5],
+        ["South Dakota", 4, 3],
+        ["Tennessee", 6, 5],
+        ["Texas", 4, 7],
+        ["Utah", 2, 4],
+        ["Vermont", 10, 1],
+        ["Virginia", 8, 4],
+        ["Washington", 1, 2],
+        ["West Virginia", 7, 4],
+        ["Wisconson", 7, 2],
+        ["Wyoming", 3, 3],
+        ["Alaska", 0, 0],
+    ];
+ 
     for (i = 0; i < 50; i++){
-	var state = states[i];
-	var newState = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-	newState.setAttribute("id", state[0]);
-	newState.setAttribute("width", String(width));
-	newState.setAttribute("height", String(width));
-	newState.setAttribute("x", String(width/2 + width*state[1]));
-	newState.setAttribute("y", String(width/2 + width*state[2]));
-	newState.setAttribute("stroke", "white");
-	s.appendChild(newState);			
-    };	   
+    var state = states[i];
+    var newState = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    newState.setAttribute("id", state[0]);
+    newState.setAttribute("width", String(width));
+    newState.setAttribute("height", String(width));
+    newState.setAttribute("x", String(width/2 + width*state[1]));
+    newState.setAttribute("y", String(width/2 + width*state[2]));
+    newState.setAttribute("stroke", "white");
+    s.appendChild(newState);
+    };
 };
 
 var resetSlider = function(){
